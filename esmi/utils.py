986b5b18862c24097ea47a63aa7f6438b00cb021
Keren Finkelstein <kfinkels@gmail.com>
2019-02-12 15:11:27 +0200

    add tuesday's events

--- esmi/utils.py
+++ esmi/utils.py
@@ -5,6 +5,25 @@ import dateparser
 
 DATE_FORMAT = '%d.%m.%Y %H:%M%p'
 
+WEEK_DAYS = {
+    'sundays': 0,
+    'mondays': 1,
+    'tuesdays': 2,
+    'wednesdays': 3,
+    'thursdays': 4,
+    'fridays': 5,
+    'saturdays': 6,
+    'sunday': 0,
+    'monday': 1,
+    'tuesday': 2,
+    'wednesday': 3,
+    'thursday': 4,
+    'friday': 5,
+    'saturday': 6,
+}
+WEEK_DAYS_REVERSE = dict(zip(WEEK_DAYS.values(), WEEK_DAYS.keys()))
+
+
 logger = logging.getLogger()
 
 
@@ -18,3 +37,14 @@ def parse_date(date: str) -> datetime.datetime:
         date: datetime = dateparser.parse(date)
     logger.debug("parsed date:{}".format(date))
     return date
+
+
+def next_weekday(timestamp, weekday):
+    days_ahead = day_str_to_int(weekday) - timestamp.weekday()
+    if days_ahead <= 0: # Target day already happened this week
+        days_ahead += 7
+    return timestamp + datetime.timedelta(days_ahead)
+
+
+def day_str_to_int(day):
+    return WEEK_DAYS[day]
